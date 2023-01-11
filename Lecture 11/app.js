8107d7eaf9b911fb36553d771f4f22a761809d8b
Saida Sainova <118192371+sadieverse@users.noreply.github.com>
2023-01-11 21:50:03 +0600

    Add files via upload

---
+++ Lecture 11/app.js
@@ -0,0 +1,36 @@
+// HOMEWORK
+
+// LECTURE 11
+
+// TASK1
+// age categories
+
+// const age = +prompt('Enter your age');
+// if (age >= 0 && age < 14){
+//     console.log('You are a child')
+// }
+// if (age >= 14 && age < 22){
+//     console.log('You are a teen')
+// }
+// if (age >= 22 && age < 30){
+//     console.log('You are young')
+// }
+// if (age >= 30 && age <= 100){
+//     console.log('You are an adult')
+// }
+
+// TASK2
+// calculate body mass index
+
+// let a = prompt('Enter your gender');
+// let b = +prompt('Enter your height');
+// let c = +prompt('Enter your waist width');
+// let d = 76 - (20*b/c);
+// let e = 64 - (20*b/c);
+// if(a = 'female'){
+//     console.log(d);
+// }
+// else{
+//     console.log(c);
+// }
+
