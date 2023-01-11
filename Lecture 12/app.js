5448d71ff85f6321a7c9a5e0ced8709a7f2dbe77
Saida Sainova <118192371+sadieverse@users.noreply.github.com>
2023-01-11 22:02:10 +0600

    Add files via upload

---
+++ Lecture 12/app.js
@@ -0,0 +1,53 @@
+// HOMEWORK
+
+// LECTURE 11
+
+// TASK1
+// age categories
+
+// const age = +prompt('Enter your age');
+// if (age >= 0 && age < 14){
+//     console.log('You are a child')
+// }
+// if (age >= 14 && age < 22){
+//     console.log('You are a teen')
+// }
+// if (age >= 22 && age < 30){
+//     console.log('You are young')
+// }
+// if (age >= 30 && age <= 100){
+//     console.log('You are an adult')
+// }
+
+// TASK2
+// calculate body mass index
+
+// let a = prompt('Enter your gender');
+// let b = +prompt('Enter your height');
+// let c = +prompt('Enter your waist width');
+// let d = 76 - (20*b/c);
+// let e = 64 - (20*b/c);
+// if(a = 'female'){
+//     console.log(d);
+// }
+// else{
+//     console.log(c);
+// }
+
+
+// LECTURE 12
+
+// TASK1
+// FIND AN AVERAGE OF TWO NUMBERS
+
+// let a = +prompt('first number','');
+// let b = +prompt('second number','');
+// console.log((a+b)/2);
+
+// TASK2
+// FIND A REMAINDER FROM TWO NUMBER'S DEVISION
+
+// let a = +prompt('first number','');
+// let b = +prompt('second number','');
+// console.log(a % b);
+
